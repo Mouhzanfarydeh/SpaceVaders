aeb0bcc138b4c91daffb59ab336e0d3b33849129
Paul <paulwerzinger@gmail.com>
2021-01-05 11:53:25 +0100

    Pauls Update
    New Lasers
    New colliders
    + some minor changes

---
+++ Assets/Scripts/NewEnemyFire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEnemyFire : MonoBehaviour
+{
+    //Bullet
+    [Header("Player Bullets")]
+    public Transform[] bulletSpawns;
+    double nextFireBullet;
+    int bulletLevel = 1; // Anzahl der Schüsse, später noch anpassbar
+    int bulletDamage = 1; // Später noch anpassbar
+    public double fireRate = 0.5; // Später noch anpassbar
+    public GameObject bullet;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( /* isDragged &&  */ Time.time > nextFireBullet) //&& !isDead)
+        {
+            nextFireBullet = Time.time + fireRate;
+            for (int i = 0; i < bulletLevel; i++)
+            {
+                GameObject newBullet = Instantiate(bullet, bulletSpawns[i].position, bulletSpawns[i].rotation) as GameObject;
+                // Bringt der Bullet Schaden
+                newBullet.GetComponent<PlayerBullet>().SetDamage(bulletDamage);
+            }
+        }
+    }
+}
