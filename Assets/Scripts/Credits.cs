93294b9604f0e544275f1bb283c82a22ac99ec87
WilliamKieburg <battle-franky@hotmail.de>
2021-02-05 15:05:50 +0100

    Update
    Menü

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
+    
     void Update()
     {
+        /*
         if(Input.GetMouseButton(0)) //der erste klick der Maus
         {
             SceneManager.LoadScene("Menu Jul");
         }
-
+        */
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("Menu Jul");
         }
     }
+    
 }
