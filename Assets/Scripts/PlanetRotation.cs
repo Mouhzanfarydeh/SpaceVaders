5d72b1416650352f3cf11e8bd3cba07942091960
Paul <paulwerzinger@gmail.com>
2021-02-08 12:51:32 +0100

    Pauls Update
    +BossSmoke
    +MineExplosion
    +PlanetRotation

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    public float RotationSpeed = .001f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(0f, 0f, RotationSpeed);
+    }
+}
