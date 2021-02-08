af94b4f0c0a0d620b0a9c53978b9d5c32691de4e
WilliamKieburg <battle-franky@hotmail.de>
2021-02-08 16:37:14 +0100

    Update
    Menü

---
+++ Assets/PlayGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) //der erste klick der Maus
+        {
+            SceneManager.LoadScene("Stage1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Stage1");
+        }
+    }
+}
