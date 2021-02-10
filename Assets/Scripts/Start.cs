eaa4ec23f614ddb55f193dc433e9bbd3b1759861
WilliamKieburg <battle-franky@hotmail.de>
2021-02-10 18:55:14 +0100

    Update

--- Assets/Scripts/Start.cs
+++ Assets/Scripts/Start.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Star : MonoBehaviour
+public class Start : MonoBehaviour
 {
 
         void Update()
