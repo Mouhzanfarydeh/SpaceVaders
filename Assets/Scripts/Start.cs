be4c2a4a84a1ddceecbdaba5bc89967b9715506a
WilliamKieburg <battle-franky@hotmail.de>
2021-02-10 18:51:08 +0100

    Start script

---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Star : MonoBehaviour
+{
+
+        void Update()
+        {
+            if (Input.GetMouseButton(0)) //der erste klick der Maus
+            {
+                SceneManager.LoadScene("Stage1");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Stage1");
+            }
+        }
+    
+}
